d89cd9a79901c66d2b2193ad4d29f1b0f961c985
Kevin Washington <kevw12188@gmail.com>
2015-09-15 11:05:51 -0400

    solved problem

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,8 +1,30 @@
+import java.util.ArrayList;
+
 
 public class Algorithm {
 
     public String ensureTestsConfigured() {
         return "Tests are configured";
+    }
+
+    public ArrayList <Integer> findSpecifiedAmountOfPrimeNumbers(int specifier) {
+        ArrayList <Integer> primeNumberList = new ArrayList<>();
+        for(int numberToCheck = 2; specifier != primeNumberList.size(); numberToCheck++) {
+            if(isPrime(numberToCheck)) {
+                primeNumberList.add(numberToCheck);
+            }
+        }
+        return primeNumberList;
+    }
+
+    public Boolean isPrime(int numberToCheck) {
+        boolean result = true;
+        for(int i = 2; i < numberToCheck; i++) {
+            if(numberToCheck % i == 0) {
+                result = false;
+            }
+        }
+        return result;
     }
 
 }
--- test/AlgorithmTest.java
+++ test/AlgorithmTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class AlgorithmTest {
@@ -11,5 +13,51 @@ public class AlgorithmTest {
         String correctMessage = "Tests are configured";
         assertEquals(correctMessage, result);
     }
+
+    @Test
+    public void itShouldReturnSpecifiedAmountOfPrimeNumbers() {
+        Algorithm alg = new Algorithm();
+        int specifier = 6;
+        ArrayList <Integer> result = alg.findSpecifiedAmountOfPrimeNumbers(specifier);
+        ArrayList <Integer> correct = new ArrayList<>();
+        correct.add(2);
+        correct.add(3);
+        correct.add(5);
+        correct.add(7);
+        correct.add(11);
+        correct.add(13);
+        assertEquals(correct, result);
+    }
+
+    @Test
+    public void itShouldReturnTrueIfNumberIsAPrime() {
+        Algorithm alg = new Algorithm();
+        int primeNumber = 5;
+        boolean correct = true;
+        boolean result = alg.isPrime(primeNumber);
+        assertEquals(correct,result);
+    }
+
+    @Test
+    public void itShouldReturnFalseIfNumberIsNotAPrime() {
+        Algorithm alg = new Algorithm();
+        int primeNumber = 4;
+        boolean correct = false;
+        boolean result = alg.isPrime(primeNumber);
+        assertEquals(correct,result);
+    }
+
+    @Test
+    public void itShouldReturnTheCorrectAnswer() {
+        Algorithm alg = new Algorithm();
+        int specifier = 10001;
+        ArrayList <Integer> primeNumberList = alg.findSpecifiedAmountOfPrimeNumbers(specifier);
+        int correctAnswer = 104743;
+        int indexOfLastItem = primeNumberList.size() - 1;
+        int lastItem = primeNumberList.get(indexOfLastItem);
+        assertEquals(correctAnswer, lastItem);
+    }
+
+
 
 }
